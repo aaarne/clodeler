a9744268f86893829b84e7d7e01adc3817bd7681
Arne Sachtler <arne.sachtler@gmail.com>
2021-11-21 18:00:43 +0100

    Add an example

--- src/clodeler/core.clj
+++ src/clodeler/core.clj
@@ -3,13 +3,36 @@
   (:require [scad-clj.scad :as scad])
   (:use [scad-clj.model]))
 
+(def cube-diameter 40)
+(def screw-hole-diameter 6.2)
+(def steckschluessel-diameter 16)
+
+(def negative
+  (union
+    (->> (cylinder (/ screw-hole-diameter 2) 50)
+         (with-center false)
+         (with-fn 120))
+    (->> (cylinder (/ steckschluessel-diameter 2) 40)
+         (with-center false)
+         (with-fn 120)
+         (translate [0 0 10]))))
+
+(def positive
+  (union
+    (->> (import "parts/dp_attachment.stl")
+         (rotate [0 (deg->rad 180) 0])
+         (translate [15 -1 35]))
+    (->> (cube cube-diameter cube-diameter cube-diameter)
+         (translate [0 0 20]))))
+
 (def model
-  (cube 10 10 10))
+  (difference positive negative))
 
 (defn write-scad
   [model]
   (spit "model.scad" (scad/write-scad model)))
 
 (defn -main
-  []
+  "write main model to scad file"
+  [& args]
   (write-scad model))
